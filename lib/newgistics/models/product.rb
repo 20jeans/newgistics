4530ca2592723e409dcfc6ef1bc3d960455c8f25
brianthecoder <brian@20jeans.com>
2014-08-21 13:42:15 -0700

    add product description

--- lib/newgistics/models/product.rb
+++ lib/newgistics/models/product.rb
@@ -2,8 +2,9 @@ module Newgistics
   class Product
     include ActiveModel::Validations
 
-    attr_accessor :sku
+    attr_accessor :name, :sku
 
+    validates :name, presence: true
     validates :sku, presence: true
   end
 end

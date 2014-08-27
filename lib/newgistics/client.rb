9813a8b7faaf731e788c6d67b9e7033b9841a7de
brianthecoder <brian@20jeans.com>
2014-08-27 12:34:28 -0700

    refactor requests

--- lib/newgistics/client.rb
+++ lib/newgistics/client.rb
@@ -23,36 +23,28 @@ module Newgistics
     end
 
     def create_products(products)
-      self.last_request = ProductRequest.new(products)
-      return last_request.errors unless last_request.valid?
-      self.last_response = client.post do |req|
-        req.url '/post_products.aspx'
-        req.body = last_request.render
-      end
-      ProductResponse.new(last_response)
+      request('/post_products.aspx', ProductRequest, ProductResponse)
     end
 
     def create_shipment(shipment)
-      self.last_request = ShipmentRequest.new(products)
-      return last_request.errors unless last_request.valid?
-      self.last_response = client.post do |req|
-        req.url '/post_shipments.aspx'
-        req.body = last_request.render
-      end
-      ShipmentResponse.new(last_response)
+      request('/post_shipments.aspx', ShipmentRequest, ShipmentResponse)
     end
 
     def create_return(rma)
-      self.last_request = ReturnRequest.new(products)
-      return last_request.errors unless last_request.valid?
-      self.last_response = client.post do |req|
-        req.url '/post_inbound_returns.aspx'
-        req.body = last_request.render
-      end
-      ReturnResponse.new(last_response)
+      request('/post_inbound_returns.aspx', ReturnRequest, ReturnResponse)
     end
 
     protected
+
+    def request(url, request_class, response_class)
+      self.last_request = request_class.new(products)
+      return last_request.errors unless last_request.valid?
+      self.last_response = client.post do |req|
+        req.url url
+        req.body = last_request.render
+      end
+      response_class.new(last_response)
+    end
 
     def client
       @_client ||= Faraday.new (@test ? TEST_URL : LIVE_URL) do |conn|

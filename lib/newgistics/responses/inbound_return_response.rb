fd51c74511fa84be44d876b3e3f0af847b21ed03
Conrad Steenberg <conrad@dstld.la>
2015-11-18 17:17:13 -0800

    Add inbound response file

---
+++ lib/newgistics/responses/inbound_return_response.rb
@@ -0,0 +1,26 @@
+module Newgistics
+  class InboundReturnResponse < Newgistics::Response
+    # @return [String] the shipping ID for the return
+    def shipping_id
+      doc.attr('shipmentID')
+    end
+
+    # @return [String] the order ID for the return
+    def order_id
+      mo = doc.css('OrderID').text.match(/(R\d{9})\W*([\w^_]*)/)
+      mo.nil? ? nil : mo[1]
+    end
+
+    def timestamp
+      Time.xmlschema(doc.css('CreateTimestamp').text)
+    end
+
+    # @return [Array][ReturnedItem] Returned Items
+    def items
+      doc.css('Item').map{|item|
+        Newgistics::ReturnedItemResponse.new(item)
+      }
+    end
+
+  end
+end
